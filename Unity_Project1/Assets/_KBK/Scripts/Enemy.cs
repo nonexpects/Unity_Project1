81b7a2cb13b93c91a8b8ce184023a8080bef9791
orionx6x <orionx6x@gmail.com>
2020-06-08 17:20:18 +0900

    게임 매니저 생성
    - 스코어 / 하이스코어 추가
    - PlayerPrefs 이용해서 하이스코어 저장

--- Unity_Project1/Assets/_KBK/Scripts/Enemy.cs
+++ Unity_Project1/Assets/_KBK/Scripts/Enemy.cs
@@ -6,12 +6,11 @@ public class Enemy : MonoBehaviour
 {
     //위에서 밑으로 떨어지게 한다(똥피하기 느낌)
     // 충돌처리 (에너미랑 플레이어, 에너미랑 플레이어 총알)
-
+    
     public float speed = 4f;
     
     void Start()
     {
-        
     }
     
     void Update()
@@ -24,6 +23,14 @@ public class Enemy : MonoBehaviour
         //자기자신도 없애고 충돌한 오브젝트도 없앤다.
         //Destroy(gameObject, 1f); // 2번째 인자는 duration
         Destroy(gameObject);
-        Destroy(collision.gameObject);
+        if(collision.gameObject.name == "Player")
+        {
+            
+        }
+        else
+        {
+            Destroy(collision.gameObject);
+        }
+        GameManager.instance.AddScore();
     }
 }
---
+++ Unity_Project1/Assets/_KBK/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using TMPro; //텍스트메쉬프로 쓸 시 
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    PlayerPrefs scoreData;
+    public Text scoreText;
+    public Text highScoreText;
+    //public TextMeshProGUI textTxt; //텍스트메쉬프로 텍스트
+
+    private string keyString = "HighScore";
+    int score;
+    int highscore;
+
+    public GameObject player;
+
+    private void Awake()
+    {
+        if (!instance)
+            instance = this;
+        highscore = PlayerPrefs.GetInt(keyString, 0);
+    }
+
+    void Start()
+    {
+        highScoreText.text = "HIGHSCORE : " + highscore.ToString();
+        scoreText.text = "SCORE : " + score;
+        
+    }
+
+    public void AddScore(int num = 1)
+    {
+        score += num;
+        scoreText.text = "SCORE : " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (score >= highscore)
+        {
+            highscore = score;
+            highScoreText.text = "HIGHSCORE : " + highscore.ToString();
+        }
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetInt(keyString, highscore);
+    }
+
+}
